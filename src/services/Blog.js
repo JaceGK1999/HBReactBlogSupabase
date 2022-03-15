6ecdf1c52a3b74daa11c37e0762cd6aadaaf6639
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-14 17:07:31 -0700

    have blogs in logs

--- src/services/Blog.js
+++ src/services/Blog.js
@@ -1,6 +1,7 @@
-import client, { checkError } from './client';
+import { client, checkError } from './client';
 
 export async function fetchBlogs() {
   const resp = await client.from('blogs').select('*');
+  console.log(resp, 'fetch response');
   return checkError(resp);
 }
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,5 +1,23 @@
-import React from 'react';
+// import React from 'react';
+import { useState, useEffect } from 'react';
+import { fetchBlogs } from '../../services/Blog';
+// import BlogCard from '../components/BlogCard/BlogCard';
 
 export default function Main() {
-  return <div>Main</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const resp = await fetchBlogs();
+        console.log(resp, 'response');
+        setBlogs(resp);
+      } catch (e) {
+        alert(e.message);
+      }
+    };
+    fetch();
+  }, []);
+  console.log(blogs, 'blogs');
+  return <div />;
 }
