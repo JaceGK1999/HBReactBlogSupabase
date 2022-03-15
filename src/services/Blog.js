97df1cf34c9a8509bacfa4af6f839779849a3a85
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-14 17:58:00 -0700

    removed log

--- src/services/Blog.js
+++ src/services/Blog.js
@@ -2,6 +2,5 @@ import { client, checkError } from './client';
 
 export async function fetchBlogs() {
   const resp = await client.from('blogs').select('*');
-  console.log(resp, 'fetch response');
   return checkError(resp);
 }
